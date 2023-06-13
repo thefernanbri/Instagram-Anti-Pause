789f44a6f419d930a8d839f087cdebd3d15495c7
thefernanbri <britofernandopro@hotmail.com>
2023-06-13 16:28:03 -0300

    Create Instagram anti pause video switch.user.js

---
+++ Instagram anti pause video switch.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Instagram anti pause video switch
+// @version      1.0
+// @description  Instagram anti pause video if tab is switched
+// @author       TheFernanbri (Fernando Brito)
+// @match        http://*.instagram.com/*
+// @match        https://*.instagram.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instagram.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+(function() {
+   'use strict';
+   window.addEventListener("visibilitychange", function(event) {
+          event.stopImmediatePropagation();
+   }, true);
+
+})();
+})();
